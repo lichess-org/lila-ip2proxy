95f1dbbc0bd5e78042f33656daa4ffdfedd94dc3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-05-09 10:20:50 +0200

    silence unzip

--- update-ip2proxy.sh
+++ update-ip2proxy.sh
@@ -3,5 +3,5 @@ mkdir -p /var/local/ip2proxy
 cd /var/local/ip2proxy
 wget -q "https://www.ip2location.com/download?token=[REDACTED]&file=PX2BIN" -O PX2-IP-PROXYTYPE-COUNTRY.BIN.ZIP
 mv IP2PROXY-IP-PROXYTYPE-COUNTRY.BIN IP2PROXY-IP-PROXYTYPE-COUNTRY.BIN.bak || echo First download
-unzip -o PX2-IP-PROXYTYPE-COUNTRY.BIN.ZIP
+unzip -q -o PX2-IP-PROXYTYPE-COUNTRY.BIN.ZIP
 systemctl restart lila-ip2proxy
