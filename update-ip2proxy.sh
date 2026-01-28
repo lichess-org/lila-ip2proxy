32801d60c00144b1bdeb8543c9f7bac3d4b5b3d4
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-01-28 23:09:50 +0100

    build: print .zip size and checksum for debugging

--- update-ip2proxy.sh
+++ update-ip2proxy.sh
@@ -7,6 +7,10 @@ mkdir -p "$LILA_IP2PROXY_DATA_DIR"
 cd "$LILA_IP2PROXY_DATA_DIR"
 
 wget -q "https://www.ip2location.com/download?token=${LILA_IP2PROXY_UPDATE_TOKEN}&file=${UPDATE_FILE}" -O "${DB_FILE}.ZIP"
+
+du -h "${DB_FILE}.ZIP"
+sha256sum "${DB_FILE}.ZIP"
+
 mv "$DB_FILE" "${DB_FILE}.bak" 2>/dev/null || true
 unzip -q -o "${DB_FILE}.ZIP"
 rm -f "${DB_FILE}.ZIP"
